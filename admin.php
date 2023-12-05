9ce1f30f982942e18c46a313ad1941b6173e0bbd
Daniel Alberto Rocha Bartaburu <89049112+xDARBx@users.noreply.github.com>
2023-12-05 18:27:44 -0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,43 @@
+<?php
+
+require "pages/header.php";
+
+?>
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="./css/admin.css">
+  </head>
+  <body>
+    <div class="center">
+      
+      <form method="post">
+        <div class="txt_field">
+          <input type="text" required>
+          <span></span>
+          <label>Usuario</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" required>
+          <span></span>
+          <label>Password</label>
+        </div>
+        <div class="pass">Olvidó su contraseña?</div>
+        <input type="submit" value="Login">
+        <div class="signup_link">
+          No es miembro? <a href="#">Registrarse</a>
+        </div>
+      </form>
+    </div>
+    <div class="espacio">
+
+    </div>
+  </body>
+</html>
+
+<?php
+require "pages/footer.php";
+?>
