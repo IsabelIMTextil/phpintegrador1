b711cbac09952fe8d8027db4b05c34ff7ea0792e
IsabelIMTextil <imtextil@gmail.com>
2023-12-17 15:52:11 -0300

    texto del noa y cards

--- pages/main.php
+++ pages/main.php
@@ -2,36 +2,36 @@
         <div class="container-fluid">
        
             <!--  empieza  carousel -->
-            <div class="row mt-1">
+            <div class="row mt-1" id="carru">
               <div class="col-12">
             <div id="carouselExample" class="carousel slide">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="imagenes/wichi.jpg" class="d-block w-100" alt="Textiles Wichí" style="height: 400px;">
+                    <img src="imagenes/wichi.jpg" class="d-block w-100" alt="Textiles Wichí" style="height:300px;">
                     <div class="carousel-caption text-right caption-center">
                       <h2 class="bg-dark">Textiles tradicionales del territorio argentino</h2>
                       <p>Charlas sobre cultura, lengua y cosmovisión de los pueblos originarios de Argentina. Talleres sobre técnicas textiles Wichí, Mapuche y del NOA.</p>
-                      <button type="button" class="btn btn-outline-warning" > <a href="#talleres">Para dar charlas</a> </button>
-                      <button type="button" class="btn btn-outline-success"><a href="#talleres">Inscripciones</a></button>
+                      <button type="button" class="btn btn-outline-warning" >Para dar charlas</button>
+                      <button type="button" class="btn btn-outline-success">Inscripciones</button>
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="imagenes/mapuche2.jpg" class="d-block w-100" alt="Textiles Mapuches" style="height: 400px;">
+                    <img src="imagenes/mapuche2.jpg" class="d-block w-100" alt="Textiles Mapuches" style="height:300px;">
                     <div class="carousel-caption text-right caption-center">
                       <h2 class="bg-dark">Textiles tradicionales del territorio argentino</h2>
                       <p>Charlas sobre cultura, lengua y cosmovisión de los pueblos originarios de Argentina. Talleres sobre técnicas textiles Wichí, Mapuche y del NOA.</p>
-                      <button type="button" class="btn btn-outline-warning"> <a href="#talleres">Para dar charlas</a></button>
-                      <button type="button" class="btn btn-outline-success"><a href="#talleres">Inscripciones</a></button>
+                      <button type="button" class="btn btn-outline-warning">Para dar charlas</button>
+                      <button type="button" class="btn btn-outline-success">Inscripciones</button>
                     </div>
                   </div>
             
                   <div class="carousel-item">
-                    <img src="imagenes/noa2.jpg" class="d-block w-100" alt="Textiles del NOA" style="height: 400px;">
+                    <img src="imagenes/noa2.jpg" class="d-block w-100" alt="Textiles del NOA" style="height:300px;">
                     <div class="carousel-caption text-right caption-center">
                       <h2 class="bg-dark">Textiles tradicionales del territorio argentino</h2>
                       <p>Charlas sobre cultura, lengua y cosmovisión de los pueblos originarios de Argentina. Talleres sobre técnicas textiles Wichí, Mapuche y del NOA.</p>
-                      <button type="button" class="btn btn-outline-warning"> <a href="#talleres">Para dar charlas</a></button>
-                      <button type="button" class="btn btn-outline-success"> <a href="#talleres">Inscripciones</a> </button>
+                      <button type="button" class="btn btn-outline-warning">Para dar charlas</button>
+                      <button type="button" class="btn btn-outline-success">Inscripciones</button>
                     </div>
             
                   </div>
@@ -46,10 +46,11 @@
                 </button>
               </div>
             </div>
-            
+            </div>
             </div>
             <!--  acá termina carousel -->
-            </div>
+            
+
             
                 <!-- comienzan las cards -->
             <div class="body">
@@ -58,21 +59,18 @@
                     <h2>Charlas sobre pueblos originarios</h2>
                     <h5> Cultura, lengua, cosmovisión y técnicas textiles</h5>
                 </div>
-
-
-                
                   <div class="row">
-                        <div class="card-group p-3">
+                        <div class="card-group">
                              
-                        <div class="col-1"> </div>
-                        <div class="col-3 m-3">
+                        <div class="col"> </div>
+                        <div class="col m-3">
                           <div class="card-responsive">
                             <img src="imagenes/wichi-tec.jpeg" class=" img-thumbnail rounded" alt="técnica textil wichí">
-                            <div class="card-body">
-                              <button type="button" class="btn btn-warning btn-sm">Charla y vídeo <a>(en vivo)</a></button>
+                            <div class="card-body p-0">
+                              <button type="button" class="btn btn-warning btn-sm">Charla y vídeo</button>
                               <button type="button" class="btn btn-outline-success">Demostración</button>
                               <h5 class="card-title">Tejido Wichí </h5>
-                              <p class="card-text">Los tejidos de la cultura Wichí se realizan con la fibra de chaguar o caraguatá, una
+                              <p class="card-text row-4">Los tejidos de la cultura Wichí se realizan con la fibra de chaguar o caraguatá, una
                                 planta que se usa desde tiempos prehispánicos con fines... </p>
                               <!-- <p class="card-text"><small class="text-body-secondary">más...</small></p> -->
                   <?php
@@ -82,14 +80,12 @@
                             </div>
                           </div>
                         </div>
-                        <div class="col-3 m-3">
+                        <div class="col m-3">
                           <div class="card-responsive">
                             <img src="imagenes/mapuche-tec.jpeg" class="img-thumbnail rounded" alt="técnica textil mapuche">
                             <div class="card-body">
-                              <button type="button" class="btn btn-warning btn-sm">Charla <a>(en vivo)</a>
-                          
-                              </button>
-                              <button type="button" class="btn btn-outline-success">Demostración </button>
+                              <button type="button" class="btn btn-warning btn-sm">Charla</button>
+                              <button type="button" class="btn btn-outline-success">Demostración</button>
                               <h5 class="card-title">Técnica textil Mapuche</h5>
                               <p class="card-text">Los tejidos de la cultura Mapuche se realizan en telar vertical o "witral", que en
                                 lengua mapuche...</p>
@@ -102,11 +98,11 @@
                             </div>
                           </div>
                         </div>
-                        <div class="col-3 m-3">
+                        <div class="col m-3">
                           <div class="card-responsive">
                             <img src="imagenes/diaguita-tec.jpeg" class="img-thumbnail rounded" alt="técnica textil del NOA">
                             <div class="card-body">
-                              <button type="button" class="btn btn-warning btn-sm">Charla y vídeos <a>(en vivo)</a></button>
+                              <button type="button" class="btn btn-warning btn-sm">Charla y vídeos</button>
                               <button type="button" class="btn btn-outline-success" >Demostración</button>
                               <h5 class="card-title">Técnica textil del NOA</h5>
                               <p class="card-text">El tejido en telar es una de las principales técnicas textiles del NOA. Se arma una
@@ -119,7 +115,7 @@
                             </div>
                           </div>
                         </div>
-                        <div class="col-1"> </div>
+                        <div class="col"> </div>
                      
                       </div>
                     </div>
@@ -146,57 +142,62 @@
                   
                   echo "<td><a class='button [btn btn-warning btn-block]'href='pages/expositores.php' target='_blank'>Conocé a los expositores</a></td></tr>"; 
 ?>
-  </div>
+
+                    </div>
             <!-- fin conocé más -->
             
-
-
                  <!-- inicio formulario -->
             
- <div class="container">
-    <div class="row align-items-start">
-        <div class="col-sm-3">  </div>
-   <div class="col-sm-6"> 
-<!-- abrir tickets en otra pestaña -->
- <p id="talleres"></p> 
-   <h4>Talleres simultáneos con cupo limitado. <br> Adquirí tu ticket aquí: <?php echo "<td><a class='button button-tickets' href='tickets.php' target='_blank'>TICKETS</a></td>";?></h4>
-   <br>
-  <br>
-  <h3>INSCRIPCIONES PARA DAR CHARLAS</h3>
+            <div class="container">
+                    <div class="row align-items-start">
+            
+                      <div class="col-sm-3">
+                                  </div>
+                      <div class="col-sm-6">
+               
 
+<!-- abrir tickets en otra pestaña -->
+
+                        <div class="text-center">
+                        <p id="talleres"></p> 
+                        <h4>Talleres simultáneos con cupo limitado. <br> Adquirí tu ticket aquí:   <?php          echo "<td><a class='button button-tickets' href='tickets.php' target='_blank'>TICKETS</a></td>"?></h4>
+                       
+                        <br>
+                        <h3>Inscripciones para dar charlas</h3>
+                        </div>
 
                         <?php
                              //  require 'conexion.php';
                         ?>
                         <!-- agrego segú clase  -->
                         <!-- <form action="../phpintegrador1/datos1.php" method="POST"> -->
-                        <form action="../phpintegrador1/datos1.php" method="POST">
+                        <form action="datos1.php" method="POST">
                         <!-- Para que funcione en WEBHOST hay que cambiar a <form action="datos1.php" method="POST">  -->
                         
                         <div class="row" id="cupo">
                         <div class="form-group row">
-                          <input type="text" class="nombre" placeholder="nombre" name="nombre" required>
+                          <input type="text" class="form-control" placeholder="nombre" name="nombre" required>
                         </div>
                         <div class="form-group row">
-                          <input type="text" class="apellido" placeholder="apellido" name="apellido" required>
+                          <input type="text" class="form-control" placeholder="apellido" name="apellido" required>
                         </div>
                         <div class="form-group row">
-                          <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="número de teléfono" name="telefono" required>
+                          <input type="text" class="form-control"  placeholder="número de teléfono" name="telefono" required>
+                        </div>
+                        <div class="form-group row">
+                          <input type="text" class="form-control"  placeholder="correo electrónico" name="correo" required>
                         </div>
 
-                       
-                          <div class="mb-1">
-                          <label for="exampleFormControlInput1" class="form-label" > </label>
-                          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Correo electrónico" name="correo" required>
-                          </div>
-                          <div class="mb-1">
-                            <label for="exampleFormControlTextarea1" class="form-label"></label>
-                          <textarea class="form-control" id="exampleFormControlTextarea1" name="comentario" placeholder="¿Te gustaría dar una charla? Contanos." cols="1" rows="3" required></textarea>
+                        <div class="row">
+                          <textarea class="form-control"  name="comentario" placeholder="¿Te gustaría dar una charla? Contanos." required></textarea>
                           <p><small>Recuerda incluir un título para tu charla.</small></p>
-                          </div>
+                        </div>
 
-                          <input type="submit" value="Enviar">
-                          <input type="reset" value="Borrar">
+                          <div class="row">
+                          <input class="btn btn-success mb-1" type="submit" value="Enviar">
+                          <input class="btn btn-warning " type="reset" value="Borrar">
+                          </div>
+                          <br><br>
                         </form>
 <!-- termina agrego segú clase  -->
 
