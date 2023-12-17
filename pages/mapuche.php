7263274b8b4b05feb82a4af6c45bdd1707d1fa45
IsabelIMTextil <imtextil@gmail.com>
2023-12-16 21:22:21 -0300

    agregué borde a imagenes de culturas y acomodé video mapuche

--- pages/mapuche.php
+++ pages/mapuche.php
@@ -40,7 +40,7 @@
 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src="../imagenes/erme.png" class=" img-thumbnail rounded" alt="Ermelinda Painequeo" width="500"  height="500">
+      <img src="../imagenes/erme.png" class=" img-thumbnail rounded" alt="Ermelinda Painequeo" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title"  style="width: 450px;">Ermelinda Painequeo - Cultura Mapuche</h5> <p  style="width: 450px;">De Trelew, Chubut</p>
         
@@ -50,7 +50,7 @@
 
   <div class="col">
     <div class="card">
-      <img src="../imagenes/poncho.jpeg" class="img-thumbnail rounded" alt="Poncho" width="500"  height="500">
+      <img src="../imagenes/poncho.jpeg" class="img-thumbnail rounded" alt="Poncho" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body" >
         <h5 class="card-title">Patagonia</h5>
         <p class="card-text" style="width: 450px;">La Patagonia Argentina está ubicada en el extremo sur del continente americano e incluye las provincias de Río Negro, Neuquén, Chubut, Santa Cruz y Tierra del Fuego. Como región, incluye además parte de Chile. Entonces, la cordillera de los Andes se configura históricamente como un punto de encuentro y no como una frontera. En este espacio, las montaas y las mesetas se entremezclan con bosques, lagos, volcanes y glaciares. La mayoría de los tejidos de esta región son blancos y negros, pero también los hay de colores. Para teñir se usaban tintes de origen minerall, vegetal, como el calafate, un arbusto del que se aprovechan las bayas para los tonos violeetas y tallos para los amarillos. Desde hace tiempo se han incorporado anilinas industriales para lograr piezas más coloridas.</p>
@@ -63,7 +63,7 @@
 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src="../imagenes/guardaatada.jpeg" class="img-thumbnail rounded" alt="Técnica guarda atada" width="500"  height="500">
+      <img src="../imagenes/guardaatada.jpeg" class="img-thumbnail rounded" alt="Técnica guarda atada" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">Guarda atada o ikat</h5>
         <p class="card-text"  style="width: 450px;">Para teñir los tejidos, una técnica característica es la "guarda atada o ikat". Se arma la urdimbre con lana sin teñir. Luego se hacen atados pequeños con hilos de urdimbre, se los recubre con tierra blanca mezclada con agua y se amarran con fibras vegetales o lana. Después toda la urdimbre se retira del telar y se sumerge en el tinte. Una vez teñida, se vuelve a colocar en el telar, se desatan las ataduras por partes y se empieza a tejer. Los lugares a los que no llegó el color ayudan a generar patrones.</p>
@@ -73,7 +73,7 @@
   
   <div class="col">
     <div class="card">
-      <img src="../imagenes/manta.jpeg" class="img-thumbnail rounded" alt="Manta en técnica guarda atada" width="500"  height="500">
+      <img src="../imagenes/manta.jpeg" class="img-thumbnail rounded" alt="Manta en técnica guarda atada" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">El telar vertical</h5>
         <p class="card-text"  style="width: 450px;">Los tejidos de la cultura Mapuche se realizan en telar vertical o "witral", que en lengua Mapuche quiere decir "estar de pie". GEneralmente se teje afuera de la casa, con el telar apoyado en la pared. Los diseños de la tetxtilería mapuche están relacionados con la cosmovisión de este pueblo, una de las culturas originarias de la Patagonia, que constituye una unidad política, lingüística, cultural e histórica. Antiguamente se tejía principalmente con lana de guanaco, que después de la conquista fue desplazada por la oveja. Hoy en día también se utilizan fibras de cabra e hilo industrial.</p>
@@ -90,7 +90,7 @@
   
   <div class="col">
     <div class="card">
-      <img src="../imagenes/caballo.png" class="img-thumbnail rounded" alt="pelero" width="500"  height="500">
+      <img src="../imagenes/caballo.png" class="img-thumbnail rounded" alt="pelero" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">El caballo llega al sur</h5>
         <p class="card-text"  style="width: 450px;">Cuando llegaron los conquistadores españoles, uno de los cambios que introdujeron en el estilo de vida de estas comunidades fue la incorporación del caballo. A partir de entonces, la confección de tejidos ya no se destinó sólo a la vestimenta o al abrigo, sino también a los aperos para los caballos. En esas piezas textiles se sumaron otros materiales (cueros y metales) y nuevas técnicas.</p>
@@ -98,25 +98,22 @@
     </div>
   </div>
   <div class="col">
+  <div class="col">
     <div class="card">
-      <img  class="card-img-top" >
-      <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text"></p>
-      </div>
+    <iframe style="border: 1px solid #000;" class="m-auto p-1" width="100%" height="315" src="https://www.youtube.com/embed/DJC50gjxbA8?si=c-U_kEllcTc7KBP7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
+    </div>
     </div>
   </div>
 </div>
 
 
-<!-- <div class="row row-cols-1 row-cols-md-2 g-4"> -->
-<div class="col">
-    <div class="card">
-    <iframe class="m-auto p-1" width="60%" height="315" src="https://www.youtube.com/embed/DJC50gjxbA8?si=c-U_kEllcTc7KBP7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    
-    </div>
+
+<script style="border: 1px solid #000;" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 
 <br><br>
 </body>
--- pages/noa.php
+++ pages/noa.php
@@ -29,7 +29,7 @@
 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src="../imagenes/noa_manta.jpeg" class="img-thumbnail rounded" alt="manta tejia en telar criollo" width="500"  height="500">
+      <img src="../imagenes/noa_manta.jpeg" class="img-thumbnail rounded" alt="manta tejia en telar criollo" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">NOA - Noroeste Argentino</h5>
         <p class="card-text"  style="width: 450px;">El noroeste argentino está integrado por las provincias de Jujuy, Salta, Tucumán, Catamarca, La Rioja y Santiago del Estero. Esta región tuvo un importante desarrollo prehispánico. Además, cuenta con las ciudades coloniales más antiguas de nuestro país, ya que es la zona de entrada de los conquistadores españoles desde Perú. Las poblaciones locales vivieron y viven en los muy distintos paisajes: están las altas montañas y los valles fértiles, las quebradas y las sierras subandinas, las yungas y la puna. </p>
@@ -38,7 +38,7 @@
   </div>
   <div class="col">
     <div class="card">
-      <img src="../imagenes/noa_alforja.jpeg" class="img-thumbnail rounded" alt="alforja tejida" width="500"  height="500">
+      <img src="../imagenes/noa_alforja.jpeg" class="img-thumbnail rounded" alt="alforja tejida" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">Alforja</h5>
         <p class="card-text"  style="width: 450px;">Según la altura, la temperatura y la humedad, en cada lugar encontramos maís, quinoa, hojas de coca, sal o semillas de cebil, entre otros. Con todos esos recursos naturales, que conviven en pocos kilómetros de distancia, el intercambio siempre fue fundamental en las organización económica, social y política de las comunidades del NOA. Foto: alforja, que se utiliza para el transporte de cargas. Tejida en telar criollo y bordada. Lana de oveja. Realizada por Dionisia de Ramos</p>
@@ -51,7 +51,7 @@
 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src="../imagenes/noa_poncho.jpeg" class="img-thumbnail rounded" alt="poncho tejido en telar criollo" width="500"  height="500">
+      <img src="../imagenes/noa_poncho.jpeg" class="img-thumbnail rounded" alt="poncho tejido en telar criollo" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">Técnica textil del NOA</h5>
         <p class="card-text"  style="width: 450px;">El tejido en telar es una de las principales técnicas textiles del NOA. Se arma una urdimbre, conjunto de hilos paralelos, que se teje al entrecruzar la trama, hilos que cruzan perpendicularmente con los de urdimbre para formar la tela. En los telares nativos se teje solamente con las manos. A partir del modelo traído de Europa, surgió el telar criollo, que tiene un pedal para levantar los hilos de urdimbre.</p>
@@ -60,7 +60,7 @@
   </div>
   <div class="col">
     <div class="card">
-      <img src="../imagenes/noa_jerga.jpeg" class="img-thumbnail rounded" alt="jerga tejida" width="500"  height="500">
+      <img src="../imagenes/noa_jerga.jpeg" class="img-thumbnail rounded" alt="jerga tejida" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">Producción textil en el noroeste</h5>
         <p class="card-text"  style="width: 450px;">Para transformar el color original del hilo se utilizan colorantes naturales o sintéticos. Los tintes naturales son los que están presentes en células vegetales y animales, y los sintéticos se logran a través de compuestos y reacciones químicas. En algunas regiones, estos últimos son los elegidos para la producción de artesanías con alta demanda comercial. El teñido de textiles empieza con la cosecha y la recolección de las plantas tintóreas, sigue con la extracción del tinte específico y termina con la tinción. Muchas veces se agregan mordientes, sustancias que fijan o modifican el color para conseguir más tonalidades. En estos procesos participa toda la comunidad. </p>
@@ -74,7 +74,7 @@
 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src="../imagenes/noa_chuspa.jpeg" class="img-thumbnail rounded" alt="Chuspa" width="500"  height="500">
+      <img src="../imagenes/noa_chuspa.jpeg" class="img-thumbnail rounded" alt="Chuspa" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">Chuspa para guardar hojas de coca.</h5>
         <p class="card-text"  style="width: 450px;">Celebrando la madre tierra. Una práctica destacada en esta región es la celebración de la fiesta de la Pachamama, cuando las comunidades se reúnen para pedir y agradecer a la Madre Tierra. Se ofrendan a la tierra alimentos y bebidas, y se ata un hilo rojo, generalmente de lana, en las muñecas y los tobillos de las personas, en los instrumentos musicales y hasta en los animales como símbolo de protección. Foto: Chuspa realizada en lana de oveja, borlas teñidas con ikat por Natalia Calizayo, Suripugio, Jujuy.</p>
@@ -83,7 +83,7 @@
   </div>
   <div class="col">
     <div class="card">
-      <img src="../imagenes/noa_telarcintura.jpeg" class="img-thumbnail rounded" alt="faja en telar de cintura" width="500"  height="500">
+      <img src="../imagenes/noa_telarcintura.jpeg" class="img-thumbnail rounded" alt="faja en telar de cintura" width="500"  height="500" style="border: 1px solid #000;">
       <div class="card-body">
         <h5 class="card-title">Telar de cintura</h5>
         <p class="card-text"  style="width: 450px;">El telar de cintura es la técnica de tejido nativa del NOA. Foto: faja realizada en telar de cintura por Rita Peñaloza, Maimará, jujuy.</p>
@@ -95,11 +95,11 @@
 <!-- <div class="row row-cols-1 row-cols-md-2 g-4"> -->
 <div class="col">
     <div class="card">
-    <iframe class="m-auto p-1" width="60%"  height="315" src="https://www.youtube.com/embed/4D-s2LdMUls?si=7D0LxyX41QOH7adH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe style="border: 1px solid #000;" class="m-auto p-1" width="60%"  height="315" src="https://www.youtube.com/embed/4D-s2LdMUls?si=7D0LxyX41QOH7adH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
       <br><br>
         
-      <iframe class="m-auto p-1" width="60%"  height="315" src="https://www.youtube.com/embed/zSJsVMYS-HA?si=fV1_fTxPIc1oKiLT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe style="border: 1px solid #000;" class="m-auto p-1" width="60%"  height="315" src="https://www.youtube.com/embed/zSJsVMYS-HA?si=fV1_fTxPIc1oKiLT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
 </div>
 <br><br>
